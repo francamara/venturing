ce53660b8a755dbb9808a70c0611f1e9a8ccae28
francamaradev <francamaradev@gmail.com>
2021-08-11 17:10:46 -0300

    refactor

--- migrations/20210811174106-Movie.js
+++ migrations/20210811174106-Movie.js
@@ -1,41 +1,44 @@
 'use strict';
 
 module.exports = {
+
   up: async (queryInterface, Sequelize) => {
+
     await queryInterface.createTable('Movies', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
         primaryKey: true,
       },
+
       name: {
         type: Sequelize.STRING,
         required: true,
         unique: true,
       },
+
       description: {
         type: Sequelize.STRING,
         required: true,
       },
+
       year: {
         type: Sequelize.INTEGER,
         required: true,
       },
     },
+
       {
         freezeTableName: true,
         timestamps: true,
       }
+
     )
   },
 
   down: function (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    return queryInterface.dropTable('Movies', null, {});
+
+    return queryInterface.dropTable('Movies', null, {})
+
   }
 };
