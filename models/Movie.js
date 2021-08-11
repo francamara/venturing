c70ea0236f663451a0c27a25face15d693a2c42a
francamaradev <francamaradev@gmail.com>
2021-08-11 17:10:35 -0300

    refactor: movie model

--- models/Movie.js
+++ models/Movie.js
@@ -1,11 +1,40 @@
-//Movie Schema
-module.exports = (sequelize, DataTypes) => {
-    const Movie = sequelize.define('Movie', {
-        name: DataTypes.STRING,
-        description: DataTypes.string,
-        year: DataTypes.number,
-        created_at: DataTypes.datetime,
-    });
+const { Sequelize, DataTypes } = require('sequelize')
+const db = require('../database/db')
 
-    return Movie;
-}
+const movieModel = db.define(
+    'Movies',
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        year: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+    },
+    {
+        timestamps: false,
+        freezeTableName: true,
+    }
+
+)
+
+module.exports = movieModel
+
+
+// module.exports = (sequelize, DataTypes) => {
+//     const Movie = sequelize.define('Movie', {
+//         name: DataTypes.STRING,
+//         description: DataTypes.string,
+//         year: DataTypes.number,
+//         created_at: DataTypes.datetime,
+//     });
+
+//     return Movie;
+// }
+
