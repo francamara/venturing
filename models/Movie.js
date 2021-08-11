274b794ef3f9d6bde7637d35e9238b53b016b4c9
francamaradev <francamaradev@gmail.com>
2021-08-11 17:15:24 -0300

    fix: unused code removed

--- models/Movie.js
+++ models/Movie.js
@@ -26,15 +26,3 @@ const movieModel = db.define(
 
 module.exports = movieModel
 
-
-// module.exports = (sequelize, DataTypes) => {
-//     const Movie = sequelize.define('Movie', {
-//         name: DataTypes.STRING,
-//         description: DataTypes.string,
-//         year: DataTypes.number,
-//         created_at: DataTypes.datetime,
-//     });
-
-//     return Movie;
-// }
-
