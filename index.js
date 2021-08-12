d2ab31b8e6126434b02b0040d49b45c40803faa2
francamaradev <francamaradev@gmail.com>
2021-08-12 18:15:46 -0300

    feat: added body parser express function and fileupload middleware

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fileUpload = require('express-fileupload')
+
 
 // Calling routes
 const movieRoutes = require('./routes/movieRoute')
@@ -6,7 +8,12 @@ const movieRoutes = require('./routes/movieRoute')
 // Express initial setup
 const app = express()
 
+app.use(fileUpload())
+app.use(express.json({ limit: '10kb' }))
+app.use(express.urlencoded({ extended: true, limit: '10kb' }))
+
 // Mounting routes
 app.use('/api/v1/movies', movieRoutes)
+
 
 module.exports = app
