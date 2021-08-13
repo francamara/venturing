c8c8c7a2ce33d9f55300dcf19f7360396893a7e6
francamaradev <francamaradev@gmail.com>
2021-08-13 17:25:42 -0300

    feat: added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const fileUpload = require('express-fileupload')
 
 
@@ -8,12 +9,13 @@ const movieRoutes = require('./routes/movieRoute')
 // Express initial setup
 const app = express()
 
+app.use(cors())
+
 app.use(fileUpload())
 app.use(express.json({ limit: '10kb' }))
 app.use(express.urlencoded({ extended: true, limit: '10kb' }))
 
 // Mounting routes
 app.use('/api/v1/movies', movieRoutes)
-
 
 module.exports = app
